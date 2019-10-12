4b81284ba2b342f47d2a73ad64215e8998f515bb
DanielVafidis <daniel.vafidis@gmail.com>
2019-10-12 04:25:41 +0200

    first

---
+++ precache-manifest.ab9c097c5cc52f156bd96ae4ebc83e36.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7ef1db376ddef2c9254f94c1c16ddd7",
+    "url": "/React-Puzzle/index.html"
+  },
+  {
+    "revision": "f65f87d5c77321d8b833",
+    "url": "/React-Puzzle/static/css/main.ddd9ced3.chunk.css"
+  },
+  {
+    "revision": "52204c475dbf494a706e",
+    "url": "/React-Puzzle/static/js/2.49b111d9.chunk.js"
+  },
+  {
+    "revision": "f65f87d5c77321d8b833",
+    "url": "/React-Puzzle/static/js/main.f18b80f9.chunk.js"
+  },
+  {
+    "revision": "8dfecf26487499e81039",
+    "url": "/React-Puzzle/static/js/runtime-main.5ddc8a1a.js"
+  },
+  {
+    "revision": "40486cf58cf2773eb6a90e0cbb01b20a",
+    "url": "/React-Puzzle/static/media/Puzzle.40486cf5.jpg"
+  },
+  {
+    "revision": "779a69da217e5c73638f5fb4422375dd",
+    "url": "/React-Puzzle/static/media/aspects.779a69da.jpg"
+  },
+  {
+    "revision": "b07464bfc3ca991cce30b4bd81107899",
+    "url": "/React-Puzzle/static/media/avantages.b07464bf.jpg"
+  },
+  {
+    "revision": "b70f36af9351056e0056f3565dc07750",
+    "url": "/React-Puzzle/static/media/gt2100.b70f36af.jpg"
+  },
+  {
+    "revision": "1b346c09535231c01cdd2bf880e25863",
+    "url": "/React-Puzzle/static/media/header.1b346c09.jpg"
+  },
+  {
+    "revision": "c9658d6903e61ef6ba36afabf06f2423",
+    "url": "/React-Puzzle/static/media/proc-cerveau.c9658d69.jpg"
+  },
+  {
+    "revision": "5ddf6cff8a3982a9e39741c5f4fef059",
+    "url": "/React-Puzzle/static/media/technologie.5ddf6cff.jpg"
+  }
+]);
